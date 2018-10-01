8089d4a1678acc8960825ebeccc4b9e1fb60e34f
seag11 <seag11@users.noreply.github.com>
2018-10-01 11:01:29 -0700

    First working session

--- spectrum.py
+++ spectrum.py
@@ -1,7 +1,42 @@
 # Spectrum.py
 import pygame
 
-WINDOW_HEIGHT = 600   #setup file
-WINDOW_WIDTH = 400  #setup file
+RESOLUTION = (600, 400)  # width x height
 
 pygame.init()
+screen = pygame.display.set_mode(RESOLUTION, pygame.RESIZABLE)
+
+
+
+class Menu:
+    pass
+
+class Game:
+    def __init__(self):
+        pass
+
+    def startup(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+
+menu = Menu()
+game = Game()
+
+menu.
+game.startup()
+
+while(1):
+    #obtain keyboard inputs
+    game.getCommands()
+
+    #update sprites
+    game.updateSprites()
+
+    #print to screen
+    game.printScreen()
+
+
+game.shutdown()
