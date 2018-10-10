06cfba10c9942cee331d80b9fe519286dcfcb73c
shaneklumpp <klumpps@oregonstate.edu>
2018-10-10 15:51:46 -0700

    Added blue block

--- settings.py
+++ settings.py
@@ -21,13 +21,17 @@ BLUE = (0, 0, 255)
 LIGHT_RED = (255, 50, 0)
 LIGHT_GREEN = (0, 255, 0)
 
+# Map Settings
+MAX_RIGHT_WIDTH = 1500 # Maximum distance to the right that the map goes
+
 # Block Lists. These lists will be the platform lists for the entire levels 1-3
 # Block __init__(self, x, y, w, h, color):
 
 # Block list Sky (Level 1)
 # This is currently the only level
-BLOCK_LIST = [(0, 350, 475, 200, GREEN),
-              (100, 275, 100, 20, GRAY)]
+BLOCK_LIST = [(0, 350, MAX_RIGHT_WIDTH, 200, GREEN),
+              (100, 275, 100, 20, GRAY),
+              (600, 275, 100, 20, BLUE)]
 
 # Block List Forst (Level 2)
 
