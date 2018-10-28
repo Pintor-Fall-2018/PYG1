73cb33658cd8399d4766ba33bacbe94956631dc3
Tiffany Warner <warnerti@oregonstate.edu>
2018-10-28 12:52:01 -0700

    Fixed Light Animation

--- light.py
+++ light.py
@@ -10,14 +10,18 @@ class Light(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = MAX_RIGHT_WIDTH - 500  # put it at the end of the level
         self.rect.y = 320
+        self.animation_frames = 0
 
-        def animate(self):
-            self.index = (self.index + 1)
-            if self.index >= len(self.bl_images):
-                self.index = 0
-            self.image = self.bl_images[self.index]
-            self.rect.x -= 1
+    def animate(self):
+        self.index = (self.index + 1)
+        if self.index >= len(self.bl_images):
+            self.index = 0
+        self.image = self.bl_images[self.index]
+        self.image.set_colorkey(BLACK)
 
-        def update(self):
+    def update(self):
+        self.animation_frames += 1
+        if self.animation_frames > 10:
+            self.animation_frames = 0
             print("Calling animate")
             self.animate()
