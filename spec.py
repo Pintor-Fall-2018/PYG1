a0b94bd600b1dae6f9d3e0e17dbcdb07e43c4f6b
seag11 <seag11@users.noreply.github.com>
2018-11-21 23:04:54 -0800

    spec faces either direction

--- spec.py
+++ spec.py
@@ -6,16 +6,27 @@ class Spec(pygame.sprite.Sprite):
         #self.image = pygame.Surface((20,20)) #width x height
         #self.image.fill((100,100,0))
         self.step = 0
-        self.resting_spec = pygame.image.load('images/spec0.png').convert()
+        self.resting_spec_fwd = pygame.image.load('images/spec0.png').convert()
+        self.resting_spec_bwd = pygame.image.load('images/spec5.png').convert()
+        self.transition_spec = pygame.image.load('images/spec10.png').convert()
         self.resting_spec_powerup = pygame.image.load('images/spec_powerup_animations/spec0_powerup.png').convert()
-        self.resting_spec.set_colorkey([34,177,76])
+        self.resting_spec_fwd.set_colorkey([34,177,76])
+        self.resting_spec_bwd.set_colorkey([34,177,76])
         self.resting_spec_powerup.set_colorkey([34,177,76])
-        self.animations = []
-        self.animations.append(pygame.image.load('images/spec1.png').convert())
-        self.animations.append(pygame.image.load('images/spec2.png').convert())
-        self.animations.append(pygame.image.load('images/spec3.png').convert())
-        self.animations.append(pygame.image.load('images/spec4.png').convert())
-        for animation in self.animations:
+        self.transition_spec.set_colorkey([34,177,76])
+        self.animations_forward = []
+        self.animations_forward.append(pygame.image.load('images/spec1.png').convert())
+        self.animations_forward.append(pygame.image.load('images/spec2.png').convert())
+        self.animations_forward.append(pygame.image.load('images/spec3.png').convert())
+        self.animations_forward.append(pygame.image.load('images/spec4.png').convert())
+        for animation in self.animations_forward:
+            animation.set_colorkey([34,177,76])
+        self.animations_backward = []
+        self.animations_backward.append(pygame.image.load('images/spec6.png').convert())
+        self.animations_backward.append(pygame.image.load('images/spec7.png').convert())
+        self.animations_backward.append(pygame.image.load('images/spec8.png').convert())
+        self.animations_backward.append(pygame.image.load('images/spec9.png').convert())
+        for animation in self.animations_backward:
             animation.set_colorkey([34,177,76])
         self.animations_powerup = []
         self.animations_powerup.append(pygame.image.load('images/spec_powerup_animations/spec1_powerup.png').convert())
@@ -24,7 +35,7 @@ class Spec(pygame.sprite.Sprite):
         self.animations_powerup.append(pygame.image.load('images/spec_powerup_animations/spec4_powerup.png').convert())
         for animation_powerup in self.animations_powerup:
             animation_powerup.set_colorkey([34,177,76])
-        self.image = self.resting_spec
+        self.image = self.resting_spec_fwd
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 200
@@ -38,7 +49,8 @@ class Spec(pygame.sprite.Sprite):
         self.jumpTimeElapsed = 0
         self.speed = [0,0]  # [forward, backward]
         self.vertical = [8,8,7,7,6,6,5,5,4.5,4.5,4.5,4.5,3.5,2.5,2.5,2,1.5,1.5,1,1]
-        self.shadow = {"top":0.0, "bottom":0.0, "left":0.0, "right":0.0}
+        self.shadow = {"top":0.0, "bottom":0.0, "left":0.0, "right":0.0, "center":0.0}
+        self.last_direction = "forward"
 
     def update(self, powerUp, sprites, mobs, spec_x):
         self.snapShot()  #capture position from last frame
@@ -49,6 +61,7 @@ class Spec(pygame.sprite.Sprite):
             if self.fallTimer > len(self.vertical) - 1:
                 self.fallTimer = len(self.vertical) - 1
         if self.forward:  #speeds up sprite in right direction
+            self.last_direction = "forward"
             self.speedControl(powerUp)
             self.animate(powerUp)
         elif self.speed[0] is not 0: #decelerate in forward direction
@@ -56,6 +69,7 @@ class Spec(pygame.sprite.Sprite):
             if self.speed[0] < 0:
                 self.speed[0] = 0
         if self.backward: #speeds up sprite in left direction
+            self.last_direction = "backward"
             self.speedControl(powerUp)
             self.animate(powerUp)
         elif self.speed[1] is not 0:  #decelerate in backward direction
@@ -106,16 +120,34 @@ class Spec(pygame.sprite.Sprite):
         self.step += 1
         if self.step > 39:
             self.step = 0
-        if powerUp == True:
-            self.image = self.animations_powerup[int(self.step/10)]
+        if self.backward:
+            if powerUp == True:
+                self.image = pygame.transform.flip(self.animations_powerup[int(self.step/10)],True,False)
+            else:
+                if self.shadow["center"] < self.rect.center and not self.falling:
+                    self.image = self.transition_spec
+                else:
+                    self.image = self.animations_backward[int(self.step/10)]
         else:
-            self.image = self.animations[int(self.step/10)]
+            if powerUp == True:
+                self.image = self.animations_powerup[int(self.step/10)]
+            else:
+                if self.shadow["center"] > self.rect.center and not self.falling:
+                    self.image = self.transition_spec
+                else:
+                    self.image = self.animations_forward[int(self.step/10)]
 
     def resting(self, powerUp):
         if powerUp == True:
-            self.image = self.resting_spec_powerup
+            if self.last_direction == "backward":
+                self.image = pygame.transform.flip(self.resting_spec_powerup,True,False)
+            else:
+                self.image = self.resting_spec_powerup
         else:
-            self.image = self.resting_spec
+            if self.last_direction == "backward":
+                self.image = self.resting_spec_bwd
+            else:
+                self.image = self.resting_spec_fwd
 
     #captures position of last frame into dictionary
     def snapShot(self):
@@ -123,6 +155,7 @@ class Spec(pygame.sprite.Sprite):
         self.shadow["bottom"] = self.rect.bottom
         self.shadow["right"] = self.rect.right
         self.shadow["left"] = self.rect.left
+        self.shadow["center"] = self.rect.center
 
     #Print spec properties from spec.update()
     def printSpecProperties(self):
