d236b01e1854cbed99a6e3cdf08489e22acae479
seag11 <seag11@users.noreply.github.com>
2018-11-28 22:48:42 -0800

    comments

--- powerup.py
+++ powerup.py
@@ -5,7 +5,6 @@ class PowerUp(pygame.sprite.Sprite):
     def __init__(self, x, y, image_path):
         pygame.sprite.Sprite.__init__(self)
         self.image = image_path
-        #self.image = pygame.transform.scale(self.image, (20,20))  #testing
         self.image.set_colorkey([255,255,255])  #sets white to transparent
         self.rect = self.image.get_rect()
         self.rect.x = x
